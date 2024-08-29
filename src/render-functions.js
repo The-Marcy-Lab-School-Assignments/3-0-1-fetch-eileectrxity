f0d6c954746268144b8ba2fed74a2050d47e12d7
electrxety <er191712@gmail.com>
2024-08-29 13:29:50 -0400

    solved q5: rendered status

--- src/render-functions.js
+++ src/render-functions.js
@@ -30,8 +30,28 @@ export const setupPageBasics = (parentEl) => {
     return { statusDiv, usersUl, postsUl, newUserForm, newUserDiv };
 };
 
-export const renderStatus = () => {
-}
+/* QUESTION 5
+FUNCTION ARGS:
+ statusDiv: an html el of a div, this is what the func will modify
+ statusInfoObj: an obj with url, status, and ok properties. url is a str, status is a http status num, and ok is a bool
+EXPECTED FUNCTION OUTPUT:
+<!--  where statusInfoObj = { ok: response.ok.., status: response.status.., url: response.url.. } -->
+<div>
+ <h2 id="status-heading">Info on - https://jsonplaceholder.typicode.com/usersasda</h2>
+ <p id="status-code">Status code: 404, FAIL!</p>
+</div>
+*/
+export const renderStatus = (statusDiv, statusInfoObj) => { //statusInfoObj is the obj that our checkResponseStatus() func returns
+  const h2 = document.createElement('h2')
+  h2.id = 'status-heading';
+  h2.textContent = `Info on - ${statusInfoObj.url}`;
+
+  const p = document.createElement('p');
+  p.id = 'status-code';
+  p.textContent = `Status code: ${statusInfoObj.status}, ${statusInfoObj.ok ? 'OK!' : 'FAIL!'}`
+
+  statusDiv.append(h2, p); //appending newly created els to the statusDiv- noting that append was used over appendChild as the latter can only append 1 el at a time
+};
 
 export const renderUsers = () => {
 };
